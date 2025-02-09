780e9011de114e4b463cbcdc98590f3ba9445fbc
alanbrunoscience <alanbrunomath@gmail.com>
2025-02-08 23:16:32 -0300

    :sparkles: Create contato.html page

---
+++ Classes/class00_Introduction/js/script.js
@@ -0,0 +1,7 @@
+// Selecionando o elemento button pelo seu id "botao"
+const botao = document.querySelector('#botao')
+
+/* Atribuindo uma função para o evento Click */
+botao.addEventListener('click', () => {
+  alert('JavaScript Funcionando!')
+})
